d36e32d660db0ee04aceb09c368b7817122d0ab7
velrino <hi@velrino.com>
2018-01-16 00:59:58 -0200

    Estrutura de rotas e retorno por json

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
 import(
+	//"fmt"
+	"log"
+	//"database/sql"
 	"github.com/gorilla/mux"
-	"fmt"
+	//"strconv"
+	"net/http"
+	"encoding/json"
+	//_"github.com/lib/pq"
+	//"github.com/go-sql-driver/mysql"
 )
 
-func main()
-{
+func main() {
+	
+	Routes := mux.NewRouter().StrictSlash(true);
+	
+		Routes.HandleFunc("/hello", getHello).Methods("GET")
+	
+		log.Fatal(http.ListenAndServe(":3600", Routes))
+
+}
+
+type Example struct {
+	ID uint "json:*id"
+	Example string "json:*example"
+}
+
+var examples = []Example{
+	Example{ID:1, Example:"Lorem Ipsum"},
+	Example{ID:2, Example:"Lorem Ipsum"},
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(examples)
+
+	//respondWithJSON(w, http.StatusOK, p)
 
 }
