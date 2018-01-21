72f70d29b3969b0b18990c2efb706c2bf1b38598
Velrino <denismagalhaesvelrino@gmail.com>
2018-01-21 20:42:55 -0200

    String de conexão do banco de dados

--- app/config/dbs.go
+++ app/config/dbs.go
@@ -8,7 +8,7 @@ import (
 
 func Database() *gorm.DB {
 	//docker run --name mysqldocker -v /my/custom:/etc/mysql/conf.d -p 3369:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
-	db, err := gorm.Open("mysql", "root:root@tcp(localhost:3310)/rv?charset=utf8&parseTime=True")
+	db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/redventures?charset=utf8&parseTime=True")
 	if err != nil {
 		panic("failed to connect database")
 	}
